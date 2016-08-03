f661dc942badf299eb95d427bde7fb0befe16a49
lujun9972 <lujun9972@gmail.com>
2016-08-03 20:46:08 +0800

    添加自动生成README的脚本

---
+++ generate_index.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+declare -A catalog_comment_dict
+catalog_comment_dict=([calc]="关于Cacl的内容" [elisp-common]="关于elisp的内容" [org-mode]="关于org-mode的内容" [emacs-common]="其他未分类的emacs内容" [raw]="未翻译或者翻译到一半的内容")
+
+function generate_headline()
+{
+    catalog=$1
+    echo "* " $catalog
+    echo ${catalog_comment_dict[$catalog]}
+    generate_links $catalog
+}
+
+function generate_links()
+{
+    catalog=$1
+    posts=$(ls $catalog)
+    for post in $posts
+    do
+        echo "[[https://github.com/lujun9972/emacs-document/blob/master/$catalog/$post][$post]]"
+    done
+}
+
+for catalog in ${!catalog_comment_dict[*]}
+do
+    generate_headline $catalog
+done
