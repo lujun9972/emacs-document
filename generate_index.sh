c765543b5c64bf86a772d9efa66183a6349405dc
darksun <lujun9972@gmail.com>
2017-04-06 10:27:03 +0800

    很奇怪,使用git shortlog在shell-command中捕获不到输出的结果,只能使用git log代替

--- generate_index.sh
+++ generate_index.sh
@@ -11,11 +11,8 @@ function get_contributors()
 {
     echo "* Contributors"
     echo "感谢GitHub以及:"
-    # for contributor in $(git log --pretty='%an<%ae>'|grep -viEw 'darksun|lujun9972' |sort|uniq)
-    # do
-    #     echo "+ $contributor"
-    # done
-    git shortlog --summary --email |grep -viEw 'darksun|lujun9972'|cut -f2|sed -e 's/^/+ /'
+    git log --pretty='%an<%ae>'|grep -viEw 'darksun|lujun9972' |sort|uniq|sed -e 's/^/+ /'
+    # git shortlog --summary --email |grep -viEw 'darksun|lujun9972'|cut -f2|sed -e 's/^/+ /'
 }
 
 function generate_headline()
