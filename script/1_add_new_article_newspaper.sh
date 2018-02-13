6efb88795e6dd5eccfc1ffc3de484b01856e6bb6
darksun <lujun9972@gmail.com>
2018-02-13 18:08:46 +0800

    WIP

---
+++ script/1_add_new_article_newspaper.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+set -e
+source $(dirname "${BASH_SOURCE[0]}")/base.sh
+
+url="$*"
+[[ -z "${url}" ]] && read -r -p "please input the URL:" url
+baseurl=$(get-domain-from-url "${url}")
+if url-blocked-p "${baseurl}";then
+    warn "${baseurl} is blocked!"
+    exit 1
+fi
+
+# 搜索类似的文章
+echo "search simliar articles..."
+if search-similar-articles "${url}";then
+    continue-p "found similar articles"
+fi
+
+parse_url_script="parse_url_by_${0##*_}"
+response=$(${CFG_PATH}/${parse_url_script} "${url}")
+
+# 获取title
+if [[ -z "${title}" ]];then
+    title=$(echo ${response} |jq -r .title)
+    [[ "${title}" == "null" || -z "${title}" ]] && read -r -p "please input the Title:" title
+fi
+
+# 获取content
+content=$(echo ${response} |jq -r .content)
+
+echo title= "${title}"
+echo content= "${content}"
+
+cd "$(get-project-path)"
+# 生成新文章
+source_path="$(get-project-path)/raw"
+filename=$(date-title-to-filename "${title}")
+source_file="${source_path}"/"${filename}"
+
+# 使用trap删掉临时文件
+function cleanup_temp {
+    [ -e "${source_file}" ] && rm --force "${source_file}"
+    exit 0
+}
+trap cleanup_temp  SIGHUP SIGINT SIGPIPE SIGTERM
+
+
+cat > "${source_file}"<<EOF
+#+TITLE: ${title}
+#+URL: ${url}
+#+AUTHOR: $(whoami)
+#+TAGS: raw
+#+DATE: [$(date)]
+#+LANGUAGE:  zh-CN
+#+OPTIONS:  H:6 num:nil toc:t \n:nil ::t |:t ^:nil -:nil f:t *:t <:nil
+EOF
+
+if [[ -n "${content}" || "${content}" == "null" ]];then
+    echo ${response}|jq -r .content|pandoc --reference-links --reference-location=document -f html-native_divs-native_spans -t org --wrap=preserve --strip-comments --no-highlight --indented-code-classes=python >>  "${source_file}" 
+
+else
+    eval $(get-browser) "${url}"
+fi
+
+
+eval "$(get-editor) '${source_file}'"
+read -p "保存好原稿后,按回车继续" continue
+
+# 新建branch 并推送新文章
+filename=$(basename "${source_file}")
+new_branch="$(filename-to-branch add "${filename}")"
+git branch "${new_branch}" master
+git checkout "${new_branch}"
+git add "${article_directory}/${filename}"
+git commit -m "选题: ${title}" && git push -u origin "${new_branch}"
---
+++ script/base.sh
@@ -0,0 +1,212 @@
+#!/bin/echo Warinng: this library should be sourced!
+CFG_PATH=$(cd $(dirname "${BASH_SOURCE[0]}")&&pwd)
+function get-cfg-option ()
+{
+    option="$@"
+    egrep "^${option}=" ${CFG_PATH}/project.cfg |cut -d "=" -f 2-
+}
+function reset-project-path()
+{
+    export PROJECT=$(get-cfg-option ProjectRoot)
+
+    if [[ -z "${PROJECT}" || ! -d "${PROJECT}"  ]]; then
+        PROJECT=$(find $HOME -iname TranslateProject 2>/dev/null |\
+                   awk -F "TranslateProject"IGNORECASE=1 '{print $1}')
+    fi
+
+}
+
+function get-project-path()
+{
+    if [[ -z ${PROJECT} ]];then
+        reset-project-path
+    fi
+    echo ${PROJECT}
+}
+
+
+function file-translating-p ()
+{
+    local file="$*"
+    head "$file" |grep -E -i "translat|fanyi|翻译" >/dev/null 2>&1
+}
+
+function file-translating-by-me-p()
+{
+    local file="$*"
+    head "$file" |grep -E -i "translat|fanyi|翻译" |grep $(get-github-user) >/dev/null 2>&1
+}
+
+function search-similar-articles ()
+{
+    if [[ $# -eq 0 ]];then
+        cat <<EOF
+该函数可用于检查是否存在重复的文章。
+Usage: $0 文件名称
+EOF
+        return 1
+    fi
+
+    url="$*"
+    clean_url=${url%%\?*}
+    # find $(get-project-path) -type f -name "[0-9]*.md" -print0 |xargs -I{} -0 grep -i "via:" "{}" |cut -d ":" -f2- |grep -i "${clean_url}"
+    (cd $(get-project-path) && git grep "via: *${clean_url}")
+}
+
+function command-exist-p()
+{
+    command -v "$@" >/dev/null 2>/dev/null
+}
+
+function continue-p()
+{ read -p "$*,CONTINUE? [y/n]" CONT
+  case $CONT in
+      [nN]*)
+          exit 1
+          ;;
+  esac
+}
+
+function get-github-user()
+{
+    local user=$(get-cfg-option GithubUser)
+    if [[ -z ${user} ]];then
+        user=$(git config --list |grep "user.name="|awk -F "=" '{print $2}')
+    fi
+    echo ${user}
+}
+
+function get-browser()
+{
+    local browser=$(get-cfg-option Browser)
+    if [[ -z ${browser} ]];then
+        browser="firefox"
+    fi
+    echo ${browser}
+}
+
+function get-editor()
+{
+    local editor=$(get-cfg-option Editor)
+    if [[ -z ${editor} ]];then
+        editor="vi"
+    fi
+    echo ${editor}
+}
+
+function git-branch-exist-p()
+{
+    local branch="$*"
+    git branch -a |grep -E "${branch}" >/dev/null
+}
+
+function get-domain-from-url ()
+{
+    local url="$*"
+    echo "${url}"|sed 's#^\(https*://[^/]*\).*$#\1#'
+}
+
+function url-blocked-p()
+{
+    local url="$*"
+    local blocked_domain=$(get-cfg-option BlockedDomains)
+    local domain=$(get-domain-from-url "$url")
+    # echo "$blocked_domain" |grep "$domain" >/dev/null
+    [[ "${blocked_domain}" == *"${domain}"* ]] # 可以用 == 来匹配
+}
+
+function warn ()
+{
+    echo "$*" >&2
+}
+
+function git-get-current-branch ()
+{
+    git branch |grep "*" |cut -d " " -f2
+}
+
+function filename-to-branch ()
+{
+    local operation=$1
+    shift
+    local title="$*"
+    local code=$(echo "${title}"|base64 -w 0)
+    echo "${operation}-${code}"
+}
+
+# 解析branch中包含的操作类型 add/translate
+function git-branch-to-operation()
+{
+    local branch="$*"
+    local operation=$(echo "${branch}"|cut -d "-" -f1)
+    echo "${operation}"
+}
+
+# 解析branch中包含的文件名信息
+function git-branch-to-filename()
+{
+    local branch="$*"
+    local code=$(echo "${branch}" |cut -d "-" -f2)
+    echo "${code}"|base64 -d
+}
+
+# 返回branch参数中正在编辑文件的 *绝对路径*
+function git-branch-to-file-path()
+{
+    local branch="$*"
+    local filename=$(git-branch-to-filename "${branch}")
+    find "$(get-project-path)" -name "${filename}"
+}
+
+# 根据当前branch得到当前编辑的文件 *绝对路径*
+function git-current-branch-to-file-path()
+{
+    # 在子shell中操作，不要修改原work directory
+    (
+    cd "$(get-project-path)"
+    local branch=$(git-get-current-branch)
+    git-branch-to-file-path "${branch}"
+    )
+}
+
+# 根据时间以及文章title转换成标准的文件名
+function date-title-to-filename()
+{
+    title=$(echo "$*" |sed 's/[^][0-9a-zA-Z.,()‘_ -]/-/g'|sed 's/-*$//') # 特殊字符换成-号,最后的-去掉
+    echo "${title}.org"
+}
+
+# 为文件加上翻译中的标记
+function mark-file-as-tranlating()
+{
+    local filename="$*"
+    local git_user=$(get-github-user)
+    sed -i "1i translating by ${git_user}" "${filename}"
+    sed -i "/-------------------------------/,$ s/译者ID/${git_user}/g" "${filename}"
+}
+
+# 根据url和author获取作者链接
+function get-author-link()
+{
+    url="$1"
+    domain=$(get-domain-from-url "${url}")
+    author="$2"
+    # 在子shell中操作，不要影响原shell的工作目录
+    (
+        cd $(get-project-path)
+        # 选择最多的url作为author link
+        git grep -iEc "via: *${domain}|\[${author}\]"|grep ":2$"|cut -d":" -f1|xargs -I{} grep "\[a\]:" '{}' |sort |uniq -c |sort -n |tail -n 1 |cut -d":" -f2-
+        # git grep -il "${domain}"|xargs -I{} grep -il "\[${author}\]" '{}' |tail -n 1 |xargs -I{} grep '\[a\]:' '{}' |cut -d ":" -f2-
+     )
+}
+
+# 判断文件的类型是tech还是talk
+function guess-article-type()
+{
+    local article="$*"
+    if grep '```' "${article}" >/dev/null;then
+        echo "tech"
+    else
+        echo "talk"
+    fi
+}
