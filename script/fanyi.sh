fb65e5b63245eb41e5259091f6730ea408381d8f
DarkSun <lujun9972@gmail.com>
2025-02-11 12:04:47 +0800

    修改prompt

--- script/fanyi.sh
+++ script/fanyi.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 set -o errexit
-content="$*"
+content="请将我的非中文文字翻译成中文!注意翻译时保持源文本格式不变，另外不要做无畏的演绎！:$*"
 # BASEURL="https://api.deepseek.com"
 # MODEL="deepseek-chat"
 # messages="[{\"role\": \"system\", \"content\": \"你是一个好用的翻译助手，请将我的非中文文字翻译成中文。\"},
@@ -9,7 +9,7 @@ content="$*"
 # ]"
 
 # PROMPT='你是一个好用的翻译助手，请将我的英文文字翻译成中文，非英文不用翻译，翻译后请保持org-mode格式不变!".'
-PROMPT='你是一个好用的翻译助手，请将我的非中文文字翻译成中文!注意翻译时保持源文本格式不变，另外不要做无畏的演绎！".'
+PROMPT='你是一个好用的翻译助手".'
 BASEURL="https://open.bigmodel.cn/api/paas/v4"
 MODEL="glm-4v-flash"
 messages=$(jq -n --arg content "${content}" --arg prompt "$PROMPT" '[{"role": "assistant", "content": $prompt},
